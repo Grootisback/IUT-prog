732dd7ad3cbdaa8cc004f2be4319b40dd340d7bc
sebastien <seb.soveaux@hotmail.fr>
2018-12-10 10:35:25 +0100

    Update tp4.py

--- tp4.py
+++ tp4.py
@@ -5,3 +5,14 @@ def getRandomList(nb : int, mn : int, mx : int) -> list:
 
 def __compare__(l:list, s:set) -> bool:
     return len(l)==len(s) and len([ v for v in l if not v in s])==0
+
+
+    def getRegularArray2d (nl:int, nc:int, mn:int, mx:int)->list:
+    lst=[]
+    for i in range(nl):
+        ligne=[]
+        for i2 in range (nc):
+            ligne.append(randint(mn,mx))
+        lst.append(ligne)
+    return lst
+    print(getRegularArray2d(3,5,2,100))
