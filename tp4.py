721cb14b1ef43b1ff5afd08770e27ad9d295a8db
sebastien <seb.soveaux@hotmail.fr>
2018-12-10 10:44:35 +0100

    Update tp4.py

--- tp4.py
+++ tp4.py
@@ -16,3 +16,13 @@ def __compare__(l:list, s:set) -> bool:
         lst.append(ligne)
     return lst
     print(getRegularArray2d(3,5,2,100))
+
+
+    for i in range (10000):
+    nl=randint(1,10)
+    nc=randint(1,10)
+    tableau= getRegularArray2d(nl=nl,nc=nc,mn=-10,mx=10)
+    assert len(tableau)==nl, "Le nombre d'éléments presents dans les lignes n'est pas correct"
+    for i2 in range (nl):
+        assert len (tableau[i2])==nc, "le nombre d'élements présents dans les colonnes n'est pas le bon"
+print("Tout est ok")
